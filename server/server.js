79fd5ffd3991de50c3a592964f750bdf7d966f15
FinlayS <fsgm01@gmail.com>
2018-12-12 21:44:26 +0000

    S9 L119 - Add mustachJS

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Bob\'s Birthday Boy', 'Welcome to the party'));
+    socket.emit('newMessage', generateMessage('Bob\'s Birthday App', 'An Roger\'s Xmas party'));
     
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has just joined'));
 
